95c53059a5be538bdbb0686c0313c0c3f6642a28
Cassius Hill <MarcelusWork@gmail.com>
2024-10-31 21:39:33 -0600

    fixed the tree

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="flex h-screen justify-center items-center">
         <Image src="/LIGHTHOUSE_IMAGE.png" alt="Lighthouse" width={1100} height={700} className="flex absolute" />
         <Image src="/HERS.png" alt="Hers" width={50} height={50} className="flex absolute translate-x-60 translate-y-96" />
-        <Image src="/Tree.png" alt="Tree" width={300} height={200} className="flex absolute translate-x-60 translate-y-52" />
+        <Image src="/TREE.png" alt="Tree" width={300} height={200} className="flex absolute translate-x-60 translate-y-52" />
       </div>
       
     </>
