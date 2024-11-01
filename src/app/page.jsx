e5c098d3ea43278b421cefa1f71e880b5b7eb990
Cassius Hill <MarcelusWork@gmail.com>
2024-10-31 21:36:49 -0600

    added bunny

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+      <div className="h-96"></div>
       <div className="flex h-screen justify-center items-center">
-        <Image src="/LIGHTHOUSE_IMAGE.png" alt="guh" width={500} height={300} />
+        <Image src="/LIGHTHOUSE_IMAGE.png" alt="Lighthouse" width={1100} height={700} className="flex absolute" />
+        <Image src="/HERS.png" alt="Hers" width={50} height={50} className="flex absolute translate-x-60 translate-y-96" />
+        <Image src="/Tree.png" alt="Tree" width={300} height={200} className="flex absolute translate-x-60 translate-y-52" />
       </div>
       
     </>
