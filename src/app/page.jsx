ee36e21b5f48efe06edac2ac763564afba5a08c6
Cassius Hill <MarcelusWork@gmail.com>
2024-11-01 15:16:34 -0600

    nov 1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,10 +4,11 @@ export default function Home() {
   return (
     <>
       <div className="h-96"></div>
-      <div className="flex h-screen justify-center items-center">
-        <Image src="/LIGHTHOUSE_IMAGE.png" alt="Lighthouse" width={1100} height={700} className="flex absolute" />
-        <Image src="/HERS.png" alt="Hers" width={50} height={50} className="flex absolute translate-x-60 translate-y-96" />
-        <Image src="/TREE.png" alt="Tree" width={300} height={200} className="flex absolute translate-x-60 translate-y-52" />
+      <div className="flex h-screen justify-center items-center overflow-hidden relative">
+        <Image src="/STARLIGHT_IMAGE.png" alt="Lighthouse" width={1100} height={1100} className="absolute translate-x-1 translate-y-1" />
+        <Image src="/EVERY_MAN.png" alt="Everyman" width={50} height={50} className="absolute translate-x-60 translate-y-80" />
+        <p className="absolute translate-x-60 translate-y-64">1034_103124</p>
+        <Image src="/TREE.png" alt="Tree" width={300} height={300} className="absolute translate-x-60 translate-y-52" />
       </div>
       
     </>
