e69bcbb3f03e6060d0a829491620a1e19a2816ac
Cassius Hill <MarcelusWork@gmail.com>
2024-10-31 18:22:40 -0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center">
+        <Image src="/LIGHTHOUSE_IMAGE.png" alt="guh" width={500} height={300} />
+      </div>
+      
+    </>
+  )
+}
